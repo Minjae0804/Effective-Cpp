ddc0b5a9bc0e42b24454c6e880b05b396a6bfb24
minjae0804 <kums200@gmail.com>
2026-06-18 00:14:20 +0900

    update #31

--- EffectiveC++/#31.cpp
+++ EffectiveC++/#31.cpp
@@ -128,14 +128,95 @@
 	* 이 경우 핸들 클래스는 클래스 자체의 역할을 다한다. 단지 구현을 분리하기 전과 동작이 다를 뿐이다.
 	* 
 	* 핸들 클래스 외에 다른 방법도 있다. Person을 특수 형태의 추상 기본 클래스로 정의하는 것이다. 이 때 이 추상 기본 클래스를 인터페이스 클래스라고 한다.
+	* 어떠한 기능을 나타내는 인터페이스를 추상 기본 클래스로 마련해 놓고, 이 클래스로부터 파생 클래스를 만들 수 있게 하자는 아이디어이다.
+	* 인터페이스 클래스는 파생이 목적이므로 데이터 멤버가 존재하지 않고 생성자도 없으며, 단일한 가상 소멸자와 인터페이스를 구성하는 순수가상함수만 존재한다.
+	* 
+	* 인터페이스 클래스는 자바나 닷넷의 인터페이스와 흡사하나, C++은 자바나 닷넷과 달리 인터페이스 클래스에 제약을 걸지 않는다.
+	* 일례로 자바 및 닷넷의 인터페이스는 데이터 멤버나 함수 구현이 언어 차원에서 금지되어 있지만, C++은 가능하다.
+	*/
+	class Person {
+	public:
+		virtual ~Person();
+		virtual std::string name() const = 0;
+		virtual std::string birthDate() const = 0;
+		virtual std::string address() const = 0;
+		...;
+	};
+	/*
+	* 해당 클래스를 코드에 적용하려면 Person에 대한 포인터나 참조자로 프로그래밍하는 방법밖에 없다.
+	* 또한 인터페이스 클래스의 인터페이스가 수정되지 않는 한 사용자는 리컴파일할 필요가 없다.
+	* 
+	* 또한 인터페이스 클래스를 사용하기 위해서는 객체 생성 수단이 1개 이상은 존재해야 한다.
+	* 이 문제는 보통 팩토리 함수(또는 가상 생성자)를 통해 해결한다.
+	* 팩토리 함수는 주어진 인터페이스 클래스의 인터페이스를 지원하는 객체를 동적 할당하고, 해당 포인터를 반환한다.
+	* 보통 이러한 함수는 클래스 내부에 정적 멤버로 선언된다.
+	*/
+	class Person {
+	public:
+		...;
+		static std::tr1::shared_ptr<Person> create(const std::string& name, const Date& birthday, const Address& addr);
+	};
+	/*
+	* 이러한 경우 사용자 측에서는 다음과 같이 사용한다.
+	*/
+	std::string name;
+	Date dateOfBirth;
+	Address address;
+	...;
+	static std::tr1::shared_ptr<Person> pp(Person::create(name, birthday, addr));
+	...;
+	std::cout << pp->name() << " was born on " << pp->birthdDate() << " and now lives at " << pp->address();
+	/*
+	* 물론 해당 인터페이스 클래스의 인터페이스를 지원하는 구체 클래스가 어디엔가는 정의되어야 할 것이며, 실제로 실행되는 생성자가 호출되어야 함은 당연하다.
+	* 실제로 이 부분은 다른 파일에서 이루어질 것이다.
+	* 일례로 Person 클래스로부터 파생된 RealPerson이라는 구체 클래스는 자신이 상속받은 가상함수에 대한 구현부를 제공하는 식으로 정의되었을 것이다.
+	*/
+	class RealPerson :public Person {
+		std::string theName;
+		Date theBirthDate;
+		Address theAddress;
+	public:
+		RealPerson(const std::string& name, const Date& birthday, const Address& addr)
+			: theName(name), theBirthDate(birthday), theAddress(addr) {}
+		virtual ~RealPerson() {}
+		std::string name() const;
+		std::string birthDate() const;
+		std::string address() const;
+	};
+
+	std::tr1::shared_ptr<Person> Person::create(const std::string& name, const Date& birthday, const Address& addr) {
+		return std::tr1::shared_ptr<Person>(new RealPerson(name, birthday, addr));
+	}
+	/*
+	* 인터페이스 클래스를 구현하는 용도로 가장 많이 쓰이는 매커니즘이 두 가지 있는데, 일단 위의 경우가 그 중 하나이다. 
+	* 즉 인터페이스 클래스로부터 인터페이스 명세를 상속하게 한 후, 해당 인터페이스에 포함된 함수를 구현하는 것이다.
+	* 다른 하나는 다중 상속을 사용하는 것이다.
 	*/
 
-
-
+	/*
+	* 결론적으로 핸들 클래스와 인터페이스 클래스는 구현부로부터 인터페이스를 분리함으로서 파일들 내의 컴파일 의존성을 완화시킨다.
+	* 물론 단점도 존재한다. 먼저 실행시간 비용이 증가하고, 객체당 필요한 공간이 늘어난다.
+	* 
+	* 핸들 클래스의 경우 데이터 멤버 접근을 위해 포인터를 타야 하는데, 이 자체가 비용이 된다.
+	* 또한 객체 하나씩을 저장하는 데에 필요한 메모리 크기에 구현부 포인터 크기가 더해진다.
+	* 마지막으로 구현부 포인터가 동적할당된 구현부 객체를 가리키도록 해당 구현부 포인터의 초기화가 발생해야 한다.
+	* 결국 동적할당에 발생하는 연산 오버헤드에, 메모리 고갈 예외를 마주할 가능성도 높아진다.
+	* 
+	* 인터페이스 클래스의 경우 호출되는 함수가 전부 가상 함수이기 때문에, 함수 호출 시마다 가상 테이블 점프에 따르는 비용이 발생한다.
+	* 게다가 인터페이스 클래스에서 파생된 객체는 죄다 가상 테이블 포인터를 지녀야 하므로 가상 함수의 단점을 그대로 받게 된다.
+	* 
+	* 마지막으로 핸들 클래스와 인터페이스 클래스가 동시에 지니는 약점도 있는데, 인라인 적용이 매우 어렵다.
+	* 인라인이 되기 위해서는 함수 본문이 대개 헤더에 두어야 하는데, 컴파일 의존성을 떨어트리는 설계는 이와 상극이기에 그렇다.
+	* 
+	* 
+	* 물론 이런 단점이 있답시고 이러한 기법을 절대 쓰지 않는다는 것만큼 멍청한 짓거리도 없다.
+	* 일단 개발 도중에는 위와 같은 기법을 사용하고, 출시 시점에서 비용 등을 고려하여 결합도를 증가시켜야 할 필요를 고민하라.
+	*/
 }
 /*
 * 결론.
-* 1. ...
+* 1. 컴파일 의존성을 최소화하는 핵심은 정의 대신 선언에 의존케 하는 것이다. 이는 핸들 클래스나 인터페이스 클래스로 구현 가능하다.
+* 2. 라이브러리 헤더는 그 자체로 모든 것을 갖추어야 하며 선언부만 지니는 형태여야 한다. 
 */
 
 #endif
